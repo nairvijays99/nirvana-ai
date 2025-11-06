bf6dbadf1c21cca002f4dcaa0dde411c45419d0c
Vijay Sasinathan <vj@intelnut.com>
2025-11-06 23:00:05 +0530

    chore: autoformat

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,41 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+// eslint.config.mjs
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  prettierConfig,
+
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          semi: true,
+          singleQuote: true,
+          printWidth: 100,
+          trailingComma: 'es5',
+          tabWidth: 2,
+          arrowParens: 'avoid',
+          endOfLine: 'lf',
+        },
+      ],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
+
   globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'coverage/**',
+    'dist/**',
+    'node_modules/**',
   ]),
 ]);
-
-export default eslintConfig;
